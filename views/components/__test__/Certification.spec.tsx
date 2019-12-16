0bf00a65f2c674e6f06aba42d4cd98bec3cd1dbe
yvdn <thuyvy3110@users.noreply.github.com>
2019-12-16 10:54:22 +0100

    Update Certification.spec.tsx

--- views/components/__test__/Certification.spec.tsx
+++ views/components/__test__/Certification.spec.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { mountWithTheme } from '~/utils/withThemeProviders'
 import Certification from '~/views/components/Certification'
 
-describe('About', () => {
+describe('Certification', () => {
 
   it('should render without throwing an error', function () {
     const wrap = mountWithTheme(<Certification />)
