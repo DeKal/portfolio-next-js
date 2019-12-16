cfb1705e1a0b464b4db79783efa49bfdc25d3898
yvdn <thuyvy3110@users.noreply.github.com>
2019-12-16 07:24:23 +0100

    Clean code after review

--- consts/certification.js
+++ consts/certification.js
@@ -1,4 +1,5 @@
 export const CERT_ACTIVE_ITEM = [1, 2, 3, 4]
-export const leftInfo = ['12.5%', '37.5%', '62.5%', '87.5%']
-export const leftPos = ['0%', '37.5%', '62.5%', '87.5%']
-export const zIndexArr = [40, 30, 20, 10]
+export const LEFT_INFO_POS = ['12.5%', '37.5%', '62.5%', '87.5%']
+export const LEFT_POS = ['0%', '37.5%', '62.5%', '87.5%']
+export const ZINDEX = [40, 30, 20, 10]
+
