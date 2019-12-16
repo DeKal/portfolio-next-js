6af294e48f0897d58d7139512bed1ae418debeeb
yvdn <thuyvy3110@users.noreply.github.com>
2019-12-16 08:47:13 +0100

    Create Certification.spec.tsx

---
+++ views/components/__test__/Certification.spec.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { mountWithTheme } from '~/utils/withThemeProviders'
+import Certification from '~/views/components/Certification'
+
+describe('About', () => {
+
+  it('should render without throwing an error', function () {
+    const wrap = mountWithTheme(<Certification />)
+    expect(wrap).toMatchSnapshot()
+  })
+
+})
