fffc34c27e2d8d27010e052a6995b509792f24e5
Phat Ho <hhphat@apcs.vn>
2019-12-21 20:26:07 +0700

    Adding tests and change effect to Fade left and right

--- views/components/SlideIterator.js
+++ views/components/SlideIterator.js
@@ -1,14 +1,14 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
-import Zoom from 'react-reveal/Zoom'
+import Fade from 'react-reveal/Fade'
 
 const SlideIterator = ({ children }) => {
   return (
     <Fragment>
       {children.map((element, index) => (
-        <Zoom key={index} left>
+        <Fade key={index} left={index % 2 == 0} right={index % 2 != 0}>
           {element}
-        </Zoom>
+        </Fade>
       ))}
     </Fragment>
   )
