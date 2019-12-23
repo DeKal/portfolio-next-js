7364ef1b0bea6331d15567bf68afd470f52591bc
yvdn <thuyvy3110@users.noreply.github.com>
2019-12-23 13:19:05 +0100

    Update CertItem.js

--- views/components/certification/CertItem.js
+++ views/components/certification/CertItem.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import styled from 'styled-components/macro'
 import CertIcon from '~/views/components/certification/CertIcon'
 import Item from '~/views/components/certification/Item'
-import { ZINDEX } from '~/consts/certification'
 import PropTypes from 'prop-types'
 import Col from 'react-bootstrap/Col'
 
 const CertItem = ({ itemSelected, setSelectedItem, id, name }) => {
   return (
-    <Col md={3}>
-      <ListItem id={id} onClick={() => setSelectedItem(id)}>
+    <Col sm={3} md={3} xs={3} lg={3}>
+      <Item id={id} onClick={() => setSelectedItem(id)}>
         <CertIcon id={id} itemSelected={itemSelected} name={name} />
-      </ListItem>
+      </Item>
     </Col>
   )
 }
@@ -24,7 +22,3 @@ CertItem.propTypes = {
   id: PropTypes.number,
   name: PropTypes.string
 }
-
-const ListItem = styled(Item)`
-  z-index: ${({ id }) => ZINDEX[id - 1]};
-`
