0c8ad883273a7a70e02258c370a12bdaaa5241b4
Phat Ho <hhphat@apcs.vn>
2019-12-27 21:54:37 +0700

    test

--- views/components/Head.js
+++ views/components/Head.js
@@ -2,40 +2,42 @@ import React from 'react'
 import NextHead from 'next/head'
 
 const Head = () => (
-  <NextHead>
-    <title>Portfolio | Phat Ho</title>
-    <link rel="icon" href="/favicon.ico" />
-    <link rel="canonical" href="https://phatho-folio.now.sh/" />
-    <meta charSet="utf-8" />
-    <meta
-      name="keywords"
-      content="portfolio, dekal, software engineer, freelancer, cv, work"
-    />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="DeKal | Portfolio" />
-    <meta property="og:description" content="Dekal Portfolio" />
-    <meta
-      property="og:image"
-      content="https://phatho-folio.now.sh/images/cover.jpg"
-    />
-    <meta
-      property="og:image:secure_url"
-      content="https://phatho-folio.now.sh/images/cover.jpg"
-    />
-    <meta property="og:url" content="https://phatho-folio.now.sh/" />
-    <meta property="og:locale" content="en_VN" />
-    <meta property="og:site_name" content="Phat Portfolio" />
-    <meta
-      name="google-site-verification"
-      content="ofwBFRuFL3aycSJjDcrhc8hWEPKuJ7LkNCLUrsB0Sj4"
-    />
-    <meta name="robots" content="index,follow" />
-    <meta name="googlebot" content="index,follow" />
-    <script
-      async=""
-      src="https://www.googletagmanager.com/gtag/js?id=UA-102977389-1"
-    ></script>
-  </NextHead>
+  <div>
+    <NextHead>
+      <title>Portfolio | Phat Ho</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="canonical" href="https://phatho-folio.now.sh/" />
+      <meta charSet="utf-8" />
+      <meta
+        name="keywords"
+        content="portfolio, dekal, software engineer, freelancer, cv, work"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="DeKal | Portfolio" />
+      <meta property="og:description" content="Dekal Portfolio" />
+      <meta
+        property="og:image"
+        content="https://phatho-folio.now.sh/images/cover.jpg"
+      />
+      <meta
+        property="og:image:secure_url"
+        content="https://phatho-folio.now.sh/images/cover.jpg"
+      />
+      <meta property="og:url" content="https://phatho-folio.now.sh/" />
+      <meta property="og:locale" content="en_VN" />
+      <meta property="og:site_name" content="Phat Portfolio" />
+      <meta
+        name="google-site-verification"
+        content="ofwBFRuFL3aycSJjDcrhc8hWEPKuJ7LkNCLUrsB0Sj4"
+      />
+      <meta name="robots" content="index,follow" />
+      <meta name="googlebot" content="index,follow" />
+      <script
+        async=""
+        src="https://www.googletagmanager.com/gtag/js?id=UA-102977389-1"
+      ></script>
+    </NextHead>
+  </div>
 )
 
 export default Head
