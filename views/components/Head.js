91a48802f387b754a83c3ace1b24262882081b88
Phat Ho <hhphat@apcs.vn>
2019-12-27 20:38:12 +0700

    Test

--- views/components/Head.js
+++ views/components/Head.js
@@ -1,7 +1,8 @@
 import * as React from 'react'
+import NextHead from 'next/head'
 
 const Head = () => (
-  <head>
+  <NextHead>
     <title>Portfolio | Phat Ho</title>
     <link rel="icon" href="/favicon.ico" />
     <link rel="canonical" href="https://phatho-folio.now.sh/" />
@@ -36,7 +37,7 @@ const Head = () => (
       async=""
       src="https://www.googletagmanager.com/gtag/js?id=UA-102977389-1"
     ></script>
-  </head>
+  </NextHead>
 )
 
 export default Head
