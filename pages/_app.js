7c1aa818fe82ba3c4b0a3d207583ed1c50eeb3ac
Phat Ho <hhphat@apcs.vn>
2019-12-27 22:00:26 +0700

    test

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import LocalizedComponent from '~/views/localize/LocalizedComponent'
 import Loading from '~/views/original-components/Loading'
 import ErrorBoundary from '~/views/components/ErrorBoundary'
 import ThemeProviderSelector from '~/views/theme/ThemeProviderSelector'
-import MetaHead from '~/views/components/Head'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 //Import Global style
 import '~/styles/globalStyle.sass'
@@ -30,7 +30,6 @@ export default class MyApp extends App {
           <LocalizeProvider>
             <LocalizedComponent>
               <ErrorBoundary>
-                <MetaHead />
                 <Component {...pageProps} />
               </ErrorBoundary>
             </LocalizedComponent>
