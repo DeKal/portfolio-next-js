d0f1beeaa6547a6aba734d57312f039bb816b9bf
Phat Ho <hhphat@apcs.vn>
2019-12-27 22:34:01 +0700

    Move head inside _document.js

---
+++ pages/_document.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Portfolio | Phat Ho</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="canonical" href="https://phatho-folio.now.sh/" />
+          <meta charSet="utf-8" />
+          <meta
+            name="keywords"
+            content="portfolio, dekal, software engineer, freelancer, cv, work"
+          />
+          <meta property="og:title" content="DeKal | Portfolio" />
+          <meta property="og:type" content="website" />
+          <meta property="og:description" content="Dekal Portfolio" />
+          <meta
+            property="og:image"
+            content="https://phatho-folio.now.sh/images/cover.jpg"
+          />
+          <meta
+            property="og:image:secure_url"
+            content="https://phatho-folio.now.sh/images/cover.jpg"
+          />
+          <meta property="og:url" content="https://phatho-folio.now.sh/" />
+          <meta property="og:locale" content="en_VN" />
+          <meta property="og:site_name" content="Phat Portfolio" />
+          <meta
+            name="google-site-verification"
+            content="ofwBFRuFL3aycSJjDcrhc8hWEPKuJ7LkNCLUrsB0Sj4"
+          />
+          <meta name="robots" content="index,follow" />
+
+          <meta name="googlebot" content="index,follow" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
