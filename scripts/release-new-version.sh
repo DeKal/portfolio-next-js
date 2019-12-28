4860bb216b14eb96c3fae65ea1f0d3cd74e1b362
Phat Ho <hhphat@apcs.vn>
2019-12-28 09:01:14 +0700

    Fix change log is not in order

--- scripts/release-new-version.sh
+++ scripts/release-new-version.sh
@@ -1,5 +1,5 @@
 set -e
 npm version patch
-npx auto-changelog -p && git add CHANGELOG.md
+npx auto-changelog -p --sort-commits date && git add CHANGELOG.md
 git commit -m "Add CHANGELOG to new Release"
 git push origin master && git push origin master --tag
