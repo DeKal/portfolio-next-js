581980f14fd6edf8606a2b28e731da325509c8bd
Phat Ho <hhphat@apcs.vn>
2020-05-30 14:24:22 +0700

    [#270] Update Zalora experiences

--- modules/home/consts/experiences.js
+++ modules/home/consts/experiences.js
@@ -1,5 +1,4 @@
 const row1 = ['Zalora', 'Dek']
 const row2 = ['GCS', 'GCS-intern']
-const row3 = ['SmartApp', 'Brand360']
 
-export const getCompanies = () => [row1, row2, row3]
+export const getCompanies = () => [row1, row2]
