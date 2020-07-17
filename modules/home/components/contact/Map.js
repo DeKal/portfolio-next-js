1d38bf5a4a92c0f7f5c369efde5838f1f7d806b9
Phat Ho <hhphat@apcs.vn>
2020-07-17 14:37:32 +0700

    [Enhance] Optimize size of map

--- modules/home/components/contact/Map.js
+++ modules/home/components/contact/Map.js
@@ -12,11 +12,11 @@ const Container = styled.div`
   overflow: hidden;
   background-position: top center;
   background-size: cover;
-  background-image: url(/images/map.jpg);
+  background-image: url(/images/map.webp);
   border-top: solid 1px ${props => props.theme.colors.colorDarken};
 
   @media screen and (max-width: 800px) {
-    background-image: url(/images/map-mobile.jpg);
+    background-image: url(/images/map-mobile.webp);
   }
 `
 export default Map
