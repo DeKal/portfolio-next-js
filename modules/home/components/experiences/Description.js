602f24a631a965924219f35fcb9b9fc3639b1299
Phat Ho <hhphat@apcs.vn>
2020-12-24 16:04:00 +0700

    Fix stylelint

--- modules/home/components/experiences/Description.js
+++ modules/home/components/experiences/Description.js
@@ -3,6 +3,7 @@ import styled from 'styled-components/macro'
 const Description = styled.p`
   margin-left: 80px;
   margin-top: 15px;
+
   @media screen and (max-width: 1000px) {
     margin-left: 0;
     text-align: justify;
