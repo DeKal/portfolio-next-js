cd04b20f208c840f471723e46d4426de4d48139b
Phat Ho <hhphat@apcs.vn>
2020-12-24 23:26:13 +0700

    Adding bootcareer to side project

--- modules/home/consts/sideProject.js
+++ modules/home/consts/sideProject.js
@@ -1,4 +1,5 @@
-const row1 = ['Portfolio-FE', 'Portfolio-BE']
-const row2 = ['Popswap', 'LiveGuess']
+const row1 = ['BootCareer-FE', 'BootCareer-BE']
+const row2 = ['Portfolio-FE', 'Portfolio-BE']
+const row3 = ['Popswap', 'LiveGuess']
 
-export const getProjects = () => [row1, row2]
+export const getProjects = () => [row1, row2, row3]
